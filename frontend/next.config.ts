1403c3bd26578eaacf37a15959d63edb399db92f
Trupti mayee sasmal <truptisoniya7@gmail.com>
2026-07-29 15:03:17 +0530

    feat: configure Next.js rewrites to proxy API requests to the backend in development

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,12 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["127.0.0.1", "localhost"],
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL || 'http://127.0.0.1:5000'}/:path*`,
-      },
-    ];
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: `${process.env.BACKEND_URL || 'http://127.0.0.1:5000'}/:path*`,
+        },
+      ];
+    }
+    return [];
   },
 };
 
